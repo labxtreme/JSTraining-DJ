e6c88150b09a2f1f983d46d420631dcc2b6888b3
lakshaya bathla <lakshaya.bathla@gmail.com>
2019-08-22 16:16:38 +0530

    new file:   C#/Lab 1/Addition/.vs/Addition/DesignTimeBuild/.dtbcache 	new file:   C#/Lab 1/Addition/.vs/Addition/v16/.suo 	new file:   C#/Lab 1/Addition/.vs/Addition/v16/Server/sqlite3/db.lock

---
+++ C#/Lab 1/Addition/Addition/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add");
+            int num1, num2, result;
+            num1 = Int32.Parse(Console.ReadLine());
+            num2 = Int32.Parse(Console.ReadLine());
+            result = num1 + num2;
+            Console.WriteLine("result = " + result);
+        }
+    }
+}
